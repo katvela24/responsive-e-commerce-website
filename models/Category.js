426eb851da06e65aa398e7a31e6aea3c829538a1
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-04-23 14:45:18 -0400

    Setup the sequelize database and started models

---
+++ models/Category.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model { }
+
+Category.init(
+  {
+    // define columns
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+
+    },
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }
+    
+  },
+{
+  sequelize,
+    timestamps: false,
+      freezeTableName: true,
+        underscored: true,
+          modelName: 'category',
+  }
+);
+
+module.exports = Category;
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('sequelize');
+const { Sequelize } = require('./config/connection');
+require ("dotenv").config();
+
+// import sequelize connection
+const sequelize = new Sequelize (
+    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, 
+    {
+      host: "localhost", dialect: 'mysql'
+    }
+)
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+
+sequelize.sync({
+  force:true
+}) .then(() => {
+  console.log("synced");
+}) .catch((err) => {
+  console.log(err);
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
